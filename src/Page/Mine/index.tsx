874b175f27f5870a66820a47c7c1307f28cc6237
PranjalMewaraa <102512201+PranjalMewaraa@users.noreply.github.com>
2024-10-15 02:40:53 +0530

    tryMine

--- src/Page/Mine/index.tsx
+++ src/Page/Mine/index.tsx
@@ -1,115 +1,149 @@
-import React, { useRef, useState } from 'react';
-import TopNav from '../../components/v1/TopNavMine';
-import ico from "../../assets/Images/ico.png";
-import decrypt from '../../utils/decrypt';
-import gsap from 'gsap';
-import tokenty from "../../assets/Images/TokenTycoon.png";
-import cc from "../../assets/Images/CoinCollector.png";
-import bc from "../../assets/Images/Blockchainer.png";
-import cm from "../../assets/Images/CryptoMiner.png";
+import React, { useState } from "react";
+import MissionDrawer from "@/components/MissionDrawer";
+import Price from "@/components/Price";
+import UserGameDetails from "@/components/UserGameDetails";
+import { $http } from "@/lib/http";
+import { cn, compactNumber } from "@/lib/utils";
+import { uesStore } from "@/store";
+import { useUserStore } from "@/store/user-store";
+import { Mission } from "@/types/MissionType";
+import { useQuery } from "@tanstack/react-query";
+import { Loader2Icon } from "lucide-react";
+import levelConfig from "@/config/level-config";
 
-// Define the type for the ProfitBox component
-const ProfitBox: React.FC = () => {
-  return (
-    <div className="min-w-56 flex px-3 py-2 items-center justify-between gap-2 rounded-full bg-[#283140]">
-      <p className="text-white text-base">Profit per hour</p>
-      <p className="flex text-white items-center text-base gap-2">
-        <span>
-          <img src={ico} className="w-8 h-8" alt="goat_coin" />
-        </span>
-        +10
-      </p>
-    </div>
-  );
-};
-
-// Define the type for MineCard props
-interface MineCardProps {
-  name: string;
-  img: string;
+// Define types for props
+interface LockInfoProps {
+  mission: Mission;
+  userLevel: number;
 }
 
-const MineCard: React.FC<MineCardProps> = ({ name, img }) => {
-  return (
-    <div className="w-1/2 max-w-60 h-full p-1">
-      <div id='recbg' className='w-full h-fit p-2 flex-col gap-2 bg-slate-800 text-white rounded-lg'>
-        <div className='text-lg w-full text-center font-bold'>{name}</div>
-        <div className='w-full font-[ageobold] items-center py-2 text-lg flex gap-2'>
-          <img src={img} alt={name} />
-          <div className='flex flex-col gap-1 text-xs'>Profit / Hour <span className='text-sm'>+10</span></div>
-        </div>
-        <div className='flex justify-between items-center py-1 border-t border-dashed border-white'>
-          <div>LVL1</div>
-          <div className='flex gap-1 items-center'>
-            <span>
-              <img src={ico} className="w-6 h-6" alt="goat_coin" />
-            </span>
-            100
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+// LockInfo Component for displaying locked mission information
+const LockInfo: React.FC<LockInfoProps> = ({ mission, userLevel }) => (
+  <div className="flex items-center gap-2 text-[10px]">
+    <img src="/images/lock.png" alt="lock" className="object-contain w-5 h-5" />
+    <span>
+      {mission.required_user_level && mission.required_user_level > userLevel
+        ? `Mission required lvl ${mission.required_user_level}`
+        : `Mission required friends ${mission.required_friends_invitation} invited`}
+    </span>
+  </div>
+);
 
-const Mine: React.FC = () => {
-  const [balance, setBalance] = useState<number | null>(decrypt(localStorage.getItem("alkine-db-val-er")));
-  const [activeSection, setActiveSection] = useState<string>("Fun");
-  const contentRef = useRef<HTMLDivElement | null>(null);
+interface MineCardProps {
+  mission: Mission;
+  setSelectedMission: (mission: Mission) => void;
+  setOpenDrawer: (open: boolean) => void;
+  userLevel: any ;
+  totalReferals: number;
+}
 
-  const handleSectionClick = (section: string) => {
-    setBalance(balance)
-    if (contentRef.current) {
-      gsap.to(contentRef.current, {
-        x: "-100%", // Swipe out to the left
-        opacity: 0,
-        duration: 0.5,
-        onComplete: () => {
-          setActiveSection(section); // Update the active section
-          gsap.fromTo(
-            contentRef.current,
-            { x: "100%", opacity: 0 }, // Start position for the new section
-            {
-              x: "0%", // Slide in from the right
-              opacity: 1,
-              duration: 0.5,
-            }
-          );
-        },
-      });
-    }
+const MineCard: React.FC<MineCardProps> = ({ mission, setSelectedMission, setOpenDrawer, userLevel, totalReferals }) => {
+  const isLocked =
+    (mission?.required_user_level && mission.required_user_level > userLevel) ||
+    (mission.required_friends_invitation && mission.required_friends_invitation > totalReferals);
+
+  const handleClick = () => {
+    if (!mission.next_level || isLocked) return;
+    setSelectedMission(mission);
+    setOpenDrawer(true);
   };
 
   return (
-    <div className="flex flex-col items-center w-full h-full p-4 text-white">
-      <TopNav active={activeSection} handleClick={handleSectionClick} />
-      <h1 className="w-full text-white text-4xl font-[ageobold] text-center p-10 flex justify-center items-center gap-2">
-        <span><img src={ico} alt="goat_coin" /></span>
-        {balance?.toFixed(1)}
-      </h1>
-      
-      <div className='w-1/2'>
-        <ProfitBox />
-      </div>
-      <div ref={contentRef} className="h-20 w-full transition-all duration-300">
-        {activeSection === "Fun" && (
-          <div className="w-full h-dvh p-2 py-4 mt-4 overflow-y-scroll">
-            <p className='text-white text-xl px-2 font-[ageobold] py-2'>Mission</p>
-            <div className='flex flex-wrap'>
-              <MineCard name={"Token Tycoon"} img={tokenty} />
-              <MineCard name={"Coin Collector"} img={cc} />
-              <MineCard name={"BlockChainer"} img={bc} />
-              <MineCard name={"Crypto Miner"} img={cm} />
-            </div>
+    <div
+      className={cn("w-1/2 max-w-60 h-full p-1", { "opacity-40 cursor-not-allowed": isLocked })}
+      onClick={handleClick}
+    >
+      <div className="w-full h-fit p-2 flex-col gap-2 bg-slate-800 text-white rounded-lg">
+        <div className="text-lg w-full text-center font-bold">{mission.name}</div>
+        <div className="w-full items-center py-2 text-lg flex gap-2">
+          <img src={mission.image} alt={mission.name} />
+          <div className="flex flex-col gap-1 text-xs">
+            Bonus per Hour
+            <span className="text-sm">
+              <Price amount={mission.production_per_hour || `+${mission.next_level?.production_per_hour || 0}`} />
+            </span>
           </div>
-        )}
-        {activeSection === "NFT" && <div>Coming Soon</div>}
-        {activeSection === "Asset Tokenize" && (
-          <div>Asset Tokenize section content goes here.</div>
-        )}
+        </div>
+        <div className="flex justify-between items-center py-1 border-t border-dashed border-white">
+          <div>LVL {mission.next_level?.level}</div>
+          <div className="flex gap-1 items-center">
+            {isLocked ? (
+              <LockInfo mission={mission} userLevel={userLevel} />
+            ) : (
+              mission.next_level?.cost && (
+                <Price amount={compactNumber(mission.next_level?.cost)} className="text-[10px]" />
+              )
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
 };
 
-export default Mine;
+export default function Missions() {
+  const user = useUserStore();
+  const { missionTypes, totalReferals } = uesStore();
+  const [activeType, setActiveType] = useState(missionTypes?.[0]);
+  const [openDrawer, setOpenDrawer] = useState<boolean>(false);
+  const [selectedMission, setSelectedMission] = useState<Mission | null>(null);
+
+  const missions = useQuery({
+    queryKey: ["/clicker/missions", activeType?.id],
+    queryFn: () =>
+      $http.$get<Mission[]>(`/clicker/missions`, {
+        params: { type: activeType?.id },
+      }),
+    staleTime: 1000 * 60,
+    enabled: !!activeType?.id,
+  });
+
+  return (
+    <div className="flex flex-col justify-end bg-cover flex-1" style={{ backgroundImage: `url(${levelConfig.bg[user?.level?.level || 1]})` }}>
+      <div className="flex flex-col flex-1 w-full h-full px-6 pb-24 mt-12 modal-body">
+        <UserGameDetails className="mt-4" />
+        <div className="flex items-center justify-center mt-10 space-x-1 text-gradient">
+          <img src="/images/coins.png" alt="coins" className="object-contain w-14 h-14" />
+          <span className="text-3xl font-bold">{Math.floor(user.balance)?.toLocaleString()}</span>
+        </div>
+        <div className="mt-10">
+          <div className="flex gap-4">
+            {missionTypes.map((type, key) => (
+              <button
+                key={key}
+                className={cn("text-xs font-bold uppercase", {
+                  "opacity-40": activeType.id !== type.id,
+                })}
+                onClick={() => setActiveType(type)}
+              >
+                {type.name}
+              </button>
+            ))}
+          </div>
+          <div className="mt-6">
+            <div className="flex flex-wrap">
+              {missions.isLoading ? (
+                <div className="flex items-center justify-center h-full col-span-2 mt-6">
+                  <Loader2Icon className="w-12 h-12 animate-spin text-primary" />
+                </div>
+              ) : (
+                missions.data &&
+                missions.data.map((mission, key) => (
+                  <MineCard
+                    key={key}
+                    mission={mission}
+                    setSelectedMission={setSelectedMission}
+                    setOpenDrawer={setOpenDrawer}
+                    userLevel={user.level?.level}
+                    totalReferals={totalReferals}
+                  />
+                ))
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+      <MissionDrawer open={openDrawer} onOpenChange={setOpenDrawer} mission={selectedMission} />
+    </div>
+  );
+}
