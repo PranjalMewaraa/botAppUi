4af83cd8f735a76d3cb58b179fbb83017f453cdb
PranjalMewaraa <102512201+PranjalMewaraa@users.noreply.github.com>
2024-10-15 04:14:44 +0530

    c

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import Game from "./Game";
+import Leaderboard from "./pages/Leaderboard";
 
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Game/>,
+  },
+  {
+    path: "/leaderboard",
+    element: <Leaderboard/>,
   },
 ]);
 
