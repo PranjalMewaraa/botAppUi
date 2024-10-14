a74de94a44111ff87afa77483336ebb986d7995d
PranjalMewaraa <102512201+PranjalMewaraa@users.noreply.github.com>
2024-10-15 02:53:09 +0530

    s

--- src/router.tsx
+++ src/router.tsx
@@ -1,56 +1,56 @@
-import { createBrowserRouter } from "react-router-dom";
-import Layout from "./components/partials/Layout";
-import Home from "./pages/Home";
+// import { createBrowserRouter } from "react-router-dom";
+// import Layout from "./components/partials/Layout";
+// import Home from "./pages/Home";
 
-import Leaderboard from "./pages/Leaderboard";
-import Earn from "./pages/Earn";
-import Friends from "./pages/Friends";
+// import Leaderboard from "./pages/Leaderboard";
+// import Earn from "./pages/Earn";
+// import Friends from "./pages/Friends";
 
-import Wallet from "./pages/Wallet";
-import Settings from "./pages/Settings";
-import Airdrop from "./pages/Airdrop";
-import Skins from "./pages/Skins";
+// import Wallet from "./pages/Wallet";
+// import Settings from "./pages/Settings";
+// import Airdrop from "./pages/Airdrop";
+// import Skins from "./pages/Skins";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
 
-    children: [
-      {
-        path: "",
-        element: <Home />,
-      },
-      {
-        path: "leaderboard",
-        element: <Leaderboard />,
-      },
-      {
-        path: "friends",
-        element: <Friends />,
-      },
-      {
-        path: "earn",
-        element: <Earn />,
-      },
-      {
-        path: "airdrop",
-        element: <Airdrop />,
-      },
-      {
-        path: "wallet",
-        element: <Wallet />,
-      },
-      {
-        path: "setting",
-        element: <Settings />,
-      },
-      {
-        path: "skin",
-        element: <Skins />,
-      },
-    ],
-  },
-]);
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />,
+//       },
+//       {
+//         path: "leaderboard",
+//         element: <Leaderboard />,
+//       },
+//       {
+//         path: "friends",
+//         element: <Friends />,
+//       },
+//       {
+//         path: "earn",
+//         element: <Earn />,
+//       },
+//       {
+//         path: "airdrop",
+//         element: <Airdrop />,
+//       },
+//       {
+//         path: "wallet",
+//         element: <Wallet />,
+//       },
+//       {
+//         path: "setting",
+//         element: <Settings />,
+//       },
+//       {
+//         path: "skin",
+//         element: <Skins />,
+//       },
+//     ],
+//   },
+// ]);
 
-export default router;
+// export default router;
