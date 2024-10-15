f80053fbd65f094f70e49a0f1f46282e5c002dc1
PranjalMewaraa <102512201+PranjalMewaraa@users.noreply.github.com>
2024-10-15 13:48:13 +0530

    x

--- src/components/v1/TapperMultiTap/index.tsx
+++ src/components/v1/TapperMultiTap/index.tsx
@@ -250,7 +250,7 @@ const PulseButton: React.FC = () => {
         </span>
         {Math.floor(tapCount)}
       </div>
-      <div className="flex items-center absolute bottom-52 space-x-2 ">
+      <div className="flex items-center space-x-2 ">
           <img
             src="/images/coin.png"
             alt="coin"
