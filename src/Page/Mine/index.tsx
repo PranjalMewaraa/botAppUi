92d0370add65f3912834a372af84fdbf775f664a
PranjalMewaraa <102512201+PranjalMewaraa@users.noreply.github.com>
2024-10-16 02:19:53 +0530

    adjust

--- src/Page/Mine/index.tsx
+++ src/Page/Mine/index.tsx
@@ -105,7 +105,7 @@ export default function Missions() {
     setSection(section);
   }
   return (
-    <div className="flex flex-col justify-end bg-cover flex-1 text-white" >
+    <div className="h-[95%] flex flex-col justify-end bg-cover flex-1 text-white" >
       <div className="flex flex-col flex-1 w-full h-full px-6 pb-24 mt-12 modal-body">
        <TopNav active={section} handleClick={handleSectionChange}/> 
         <UserGameDetails className="mt-4"/>
