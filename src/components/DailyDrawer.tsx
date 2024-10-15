6a100b5bea8086a7a400e8c2dc2f03298eee6cc0
PranjalMewaraa <102512201+PranjalMewaraa@users.noreply.github.com>
2024-10-15 14:05:30 +0530

    we

--- src/components/DailyDrawer.tsx
+++ src/components/DailyDrawer.tsx
@@ -38,7 +38,7 @@ export default function DailyDrawer({ ...props }: DrawerProps) {
   return (
     <Drawer {...props}>
       <div>
-      <img src="/images/coins.png" alt="coins" className="mx-auto h-28" />
+      <img src="/images/coins.png" alt="coins" className="mx-auto h-20" />
       <h2 className=" text-2xl font-bold text-center">Daily Reward</h2>
       <p className="mt-2.5 text-center font-medium">
         Acquire coins for logging into the game daily without skipping
