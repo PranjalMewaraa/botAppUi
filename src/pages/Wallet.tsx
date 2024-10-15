5e923e2f288936c0155bb4ecda00578f7ec22c25
PranjalMewaraa <102512201+PranjalMewaraa@users.noreply.github.com>
2024-10-15 20:57:45 +0530

    sd

--- src/pages/Wallet.tsx
+++ src/pages/Wallet.tsx
@@ -32,7 +32,7 @@ export default function Wallet() {
   // }, [tonAddress]);
 
   return (
-    <div className="min-h-screen flex flex-col justify-end bg-cover flex-1" >
+    <div id="main_div" className="min-h-screen flex flex-col justify-end bg-cover flex-1" >
       <div className="flex flex-col flex-1 w-full h-full px-6 py-8 pb-24 mt-12 modal-body">
         <img
           src="/images/toncoin.png"
