2a55d6125d1b984a068822bef02ffab278982630
PranjalMewaraa <102512201+PranjalMewaraa@users.noreply.github.com>
2024-10-16 02:14:08 +0530

    .

--- src/components/v1/TapperMultiTap/index.tsx
+++ src/components/v1/TapperMultiTap/index.tsx
@@ -139,7 +139,7 @@ const PulseButton: React.FC = () => {
       newPulseRing,
       { scale: 0, opacity: 1 },
       {
-        scale: 1.1,
+        scale: 1,
         opacity: 0.1,
         duration: 1,
         ease: "power1.out",
