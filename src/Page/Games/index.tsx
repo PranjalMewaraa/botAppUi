38b2bb6351cea7d71dfd67dfc4acceee6c06fc98
PranjalMewaraa <102512201+PranjalMewaraa@users.noreply.github.com>
2024-10-16 02:19:05 +0530

    s

--- src/Page/Games/index.tsx
+++ src/Page/Games/index.tsx
@@ -5,7 +5,7 @@ const Games = () => {
     
   return (
    
-    <div className="flex flex-col w-full h-full p-4">
+    <div className="flex flex-col w-full h-[95%] p-4">
         <h1 className="w-full text-white text-4xl font-[ageobold] text-center p-8">Choose a Game</h1>
         <TopNav/>
     </div>
