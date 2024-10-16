07e66ad1e6c38e2302d7377a65d77812c12be783
PranjalMewaraa <102512201+PranjalMewaraa@users.noreply.github.com>
2024-10-16 12:01:17 +0530

    "Updated width classes for TopNav items"

--- src/components/v1/TopNavMine/index.tsx
+++ src/components/v1/TopNavMine/index.tsx
@@ -9,7 +9,7 @@ const TopNav: React.FC<TopNavProps> = ({ active, handleClick }) => {
   return (
     <div className="h-16 w-full p-2 mt-4 flex bg-slate-800 rounded-xl gap-2">
       <div
-        className={`h-full w-1/3 text-white font-semibold flex justify-center items-center cursor-pointer ${
+        className={`h-full w-1/4 text-white font-semibold flex justify-center items-center cursor-pointer ${
           active === "Fun" ? "bg-slate-700 rounded-lg" : ""
         }`}
         onClick={() => handleClick("Fun")}
@@ -17,7 +17,7 @@ const TopNav: React.FC<TopNavProps> = ({ active, handleClick }) => {
         Fun
       </div>
       <div
-        className={`h-full w-1/3 text-white font-semibold flex justify-center items-center cursor-pointer ${
+        className={`h-full w-1/4 text-white font-semibold flex justify-center items-center cursor-pointer ${
           active === "NFT" ? "bg-slate-700 rounded-lg" : ""
         }`}
         onClick={() => handleClick("NFT")}
@@ -25,7 +25,7 @@ const TopNav: React.FC<TopNavProps> = ({ active, handleClick }) => {
         NFT
       </div>
       <div
-        className={`h-full w-1/3 text-white font-semibold flex justify-center items-center cursor-pointer ${
+        className={`h-full w-2/4 text-white font-semibold flex justify-center  items-center cursor-pointer ${
           active === "Asset Tokenize" ? "bg-slate-700 rounded-lg" : ""
         }`}
         onClick={() => handleClick("Asset Tokenize")}
