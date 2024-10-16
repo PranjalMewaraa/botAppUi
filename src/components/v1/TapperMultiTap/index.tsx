4081b1ff65c2f5cc0058cf3056d36bb6b738051e
PranjalMewaraa <102512201+PranjalMewaraa@users.noreply.github.com>
2024-10-16 09:34:10 +0530

    d

--- src/components/v1/TapperMultiTap/index.tsx
+++ src/components/v1/TapperMultiTap/index.tsx
@@ -54,7 +54,7 @@ const PulseButton: React.FC = () => {
     setTimeout(() => {
       const fingerCount = event.touches.length;
       if(!user.UserTap(fingerCount)) return;
-      if (fingerCount > 0 && fingerCount <= 5) {
+      if (fingerCount > 0 && fingerCount < 5) {
         // Add score based on finger count
         const current = localStorage.getItem("ClicksCount");
       localStorage.setItem(
