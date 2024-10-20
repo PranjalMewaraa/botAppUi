8bdcd9e8aee4cf18143ee535813d889a2cee4b98
PranjalMewaraa <102512201+PranjalMewaraa@users.noreply.github.com>
2024-10-20 16:23:26 +0530

    s

--- src/components/v1/GameCard/index.tsx
+++ src/components/v1/GameCard/index.tsx
@@ -8,7 +8,7 @@ const GameCard: React.FC<GameProps> = ({name,fee}) => {
     <div className="w-full h-full p-1">
     <div className='w-full h-fit p-2 flex-col gap-1 bg-slate-800 text-white rounded-lg'>
         <img className='w-full max-h-36' src='https://media.wired.com/photos/62855b1bb6cfd378a30c474a/master/pass/Build-Game-Watch-It-Die-Hyper-Scape-Games.jpg'/>
-        <div className='w-full text-center font-[ageobold] text-base'>{name}</div>
+        <div className='w-full text-center font-[ageobold] text-base h-24'>{name}</div>
         <div className='w-full text-center font-[ageoReg] flex justify-center'><span><img className="h-6 w-6" src={diamond} alt="" /></span> {fee}</div>
     </div>
     </div>
