e63960b6c8f88e5fda0f47d269b57c1d23f4f748
PranjalMewaraa <102512201+PranjalMewaraa@users.noreply.github.com>
2024-10-21 18:55:18 +0530

    updated price system

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,12 +5,21 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function compactNumber(num: number) {
-  return num.toLocaleString(undefined, {
-    maximumFractionDigits: 2,
-    notation: "compact",
-  });
+export function compactNumber(number: number) {
+  const options = {
+    notation: "compact" as const,
+    compactDisplay: "short" as const,
+  };
+  const usformatter = Intl.NumberFormat("en-US", options);
+  return usformatter.format(number);
 }
+// export function compactNumber(num: number) {
+
+//   return num.toLocaleString(undefined, {
+//     maximumFractionDigits: 2,
+//     notation: "compact",
+//   });
+// }
 
 export function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
