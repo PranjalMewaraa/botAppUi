b5d07635721c713fb5d80cd66a83f4cf32f1e3c4
PranjalMewaraa <102512201+PranjalMewaraa@users.noreply.github.com>
2024-10-25 19:16:55 +0530

    d

--- src/Page/MiniGames/SlotMachineUpdated.tsx
+++ src/Page/MiniGames/SlotMachineUpdated.tsx
@@ -7,7 +7,7 @@ interface ReelProps {
 
 const Reel: React.FC<ReelProps> = ({ symbol }) => {
   return (
-    <div className="flex items-center justify-center w-24 h-36 border-2 border-gray-600 bg-gradient-to-b from-gray-400 to-gray-200 shadow-lg">
+    <div className="flex items-center justify-center w-28 h-36 border-2 border-gray-600 bg-gradient-to-b from-gray-400 to-gray-200 shadow-lg">
       <h2 className="text-3xl">{symbol}</h2>
     </div>
   );
@@ -44,6 +44,7 @@ const SlotMachine: React.FC = () => {
       // Check for win condition
       if (newReels.every((symbol) => symbol === newReels[0])) {
         console.log("Win! All three reels are the same: ", newReels[0]);
+        setMessage("Yohooo! You won the Jackpot")
       } else {
         console.log("Lost! Reels are: ", newReels);
         setMessage(loser[Math.floor(Math.random() * loser.length)])
@@ -55,12 +56,21 @@ const SlotMachine: React.FC = () => {
     <div id='main_div' className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="flex flex-col items-center mt-10">
         <h1 className='w-full font-[ageobold] text-6xl text-center my-2 text-white'>Slot Machine</h1>
-        <h3 className='w-full font-[ageobold] text-yellow-400 text-center text-xl my-2'>{message}</h3>
-        <div className="flex border-2 border-yellow-500 rounded-lg">
-          {reels.map((symbol, index) => (
-            <Reel key={index} symbol={symbol} />
-          ))}
+        <h3 className='w-full font-[ageobold] text-yellow-400 text-center text-xl mb-2'>{message}</h3>
+        <div className='flex flex-col gap-4 pt-2 px-2'>
+          <div className="flex border-2 border-yellow-500 rounded-lg">
+            {reels.map((symbol, index) => (
+              <Reel key={index} symbol={symbol} />
+            ))}
+          </div>
+          <div className='flex gap-4 justify-center items-center'>
+            <div className='flex flex-col items-center gap-1 border-yellow-300'>
+              <span>Bet:</span>
+              <div className='h-8 w-10 flex justify-center items-center text-black'>10</div>
+            </div>
+          </div>
         </div>
+       
         <button
           onClick={spinReels}
           disabled={isSpinning}
