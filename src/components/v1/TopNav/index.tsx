6ca04f3780f38963798a5ac44ddd7cea61c36fa3
PranjalMewaraa <102512201+PranjalMewaraa@users.noreply.github.com>
2024-10-26 13:13:48 +0530

    update

--- src/components/v1/TopNav/index.tsx
+++ src/components/v1/TopNav/index.tsx
@@ -111,9 +111,9 @@ const TopNav = () => {
               <Link to="/game/mine">
                 <GameCard name="Mine Escape" fee={10} src="/images/02.jpg" />
               </Link>
-              {/* <Link to="/game/slot">
+              <Link to="/game/slot">
                 <GameCard name="Slot Machine" fee={100} src="/images/02.jpg" />
-              </Link> */}
+              </Link>
             </div>
           </div>
         )}
