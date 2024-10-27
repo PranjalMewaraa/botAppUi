d1696cb5fbea45784b4812bd51941ac1577cf1c9
PranjalMewaraa <102512201+PranjalMewaraa@users.noreply.github.com>
2024-10-27 12:20:15 +0530

    "Update ComingSoon component: change setActiveIndex argument from 1 to 0 in 'Back to Home' onClick handler"

--- src/components/ui/ComingSoon.tsx
+++ src/components/ui/ComingSoon.tsx
@@ -6,7 +6,7 @@ const ComingSoon = () => {
     <div id='main_div' className=' w-full h-screen flex flex-col justify-center text-white items-center gap-4'>
         <div className="text-2xl font-[ageobold]">Coming Soon</div>
         <div className="text-lg">Stay Tuned for more updates</div>
-        <div onClick={()=>setActiveIndex(1)}>Back to Home</div>
+        <div onClick={()=>setActiveIndex(0)}>Back to Home</div>
     </div>
   )
 }
