9621119481c4baf19bfd7850ad16e052a0f76a5c
PranjalMewaraa <102512201+PranjalMewaraa@users.noreply.github.com>
2024-10-27 12:50:02 +0530

    "Removed Leaderboard, Settings, Wallet, and ComingSoon routes from routers"

--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import Game from "./Game";
-import Leaderboard from "./pages/Leaderboard";
-import Settings from "./pages/Settings";
-import Wallet from "./pages/Wallet";
 import GameDesktop from "./GameDesktop";
 import RockPaperScissors from "./Page/MiniGames/RockPaperScissor";
 import MineGame from "./Page/MiniGames/MineGame";
-import ComingSoon from "./components/ui/ComingSoon";
 import SlotMachineGamePage from "./Page/MiniGames/SlotMachineUpdated";
 
 
@@ -21,28 +17,12 @@ const router = createBrowserRouter([
     element: <GameDesktop/>,
   },
   {
-    path: "/leaderboard",
-    element: <Leaderboard/>,
-  },
-  {
-    path: "/settings",
-    element: <Settings/>,
-  },
-  {
-    path: "/wallet",
-    element: <Wallet/>,
-  },
-  {
     path: "/game/rps",
     element:<RockPaperScissors/>
   }
   ,{
     path: "/game/mine",
     element:<MineGame/>
-  },
-  {
-    path: "/soon",
-    element:<ComingSoon/>
   },
   {
     path: "/game/slot",
--- src/router2.tsx
+++ src/router2.tsx
@@ -1,12 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
-
-import Leaderboard from "./pages/Leaderboard";
-import Settings from "./pages/Settings";
-import Wallet from "./pages/Wallet";
 import GameDesktop from "./GameDesktop";
 import RockPaperScissors from "./Page/MiniGames/RockPaperScissor";
 import MineGame from "./Page/MiniGames/MineGame";
-import ComingSoon from "./components/ui/ComingSoon";
 import SlotMachineGamePage from "./Page/MiniGames/SlotMachineUpdated";
 
 
@@ -20,33 +15,18 @@ const router2 = createBrowserRouter([
     element: <GameDesktop/>,
   },
   {
-    path: "/leaderboard",
-    element: <Leaderboard/>,
-  },
-  {
-    path: "/settings",
-    element: <Settings/>,
-  },
-  {
-    path: "/wallet",
-    element: <Wallet/>,
-  },
-  {
     path: "/game/rps",
     element:<RockPaperScissors/>
-  },
-  {
+  }
+  ,{
     path: "/game/mine",
     element:<MineGame/>
-  },
-  {
-    path: "/soon",
-    element:<ComingSoon/>
   },
   {
     path: "/game/slot",
     element:<SlotMachineGamePage/>
   },
+  
   
 ]);
 
