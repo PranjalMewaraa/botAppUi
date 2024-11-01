ddf70ba38fbfe79dc9a31fb56ba7e4d10a49652c
PranjalMewaraa <102512201+PranjalMewaraa@users.noreply.github.com>
2024-11-01 16:09:18 +0530

    "Updated SplashScreen component and added CSS for stroked text effect"

--- src/components/partials/SplashScreen.tsx
+++ src/components/partials/SplashScreen.tsx
@@ -45,12 +45,13 @@ export default function SplashScreen() {
       id="main_div2"
       className="relative flex flex-col items-center text-white justify-between pt-16 bg-cover bg-center w-full max-w-lg h-[--tg-viewport-height] mx-auto"
     > 
-     
-      <div className="flex flex-col items-center w-full absolute bottom-0">
-        <div className="bottom-0 flex flex-col items-center w-full pb-6 bg-[url('/images/blur.png')] bg-cover bg-center">
-          <h1 className="text-6xl font-medium text-center uppercase text-shadow">
+       <h1 id='stroked' className=" absolute top-1/2 text-5xl font-semibold text-center uppercase  stroke-black ">
           Goat Tapper
-          </h1>
+        </h1>
+      <div className="flex flex-col items-center w-full absolute bottom-0">
+        
+        <div className="bottom-0 flex flex-col items-center w-full pb-6 bg-[url('/images/blur.png')] bg-cover bg-center">
+         
           <p className="mt-3 text-sm font-bold uppercase text-primary">
             Stay tuned for House Drop
           </p>
